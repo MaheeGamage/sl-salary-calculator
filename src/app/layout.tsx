725de847cd0912f885c8a584e29f9b976868be60
MaheeGamage <mahee.gamage@gmail.com>
2024-02-23 23:31:09 +0530

    Remove unused chakra files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import customTheme from '@/style/theme'
 import StickyFooter from '@/components/StickyFooter'
 
 const inter = Inter({ subsets: ['latin'] })
