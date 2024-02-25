8aa35614b9c8558ffa384d54b97c8f3ba3923a97
MaheeGamage <mahee.gamage@gmail.com>
2024-02-25 17:54:43 +0530

    Revert "Change base template size"
    This reverts commit ee5ac90f5bc68d78cd642469b676bdbb3f5b7bb1.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 
-import { Grid } from '@mui/material'
 import styles from './page.module.css'
 import SalaryCalc from '@/components/SalaryCalc'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
-        <Grid item xs={10} sm={8} md={6}>
-          <SalaryCalc />
-        </Grid>
-      </Grid>
+      <SalaryCalc />
     </main>
   )
 }
