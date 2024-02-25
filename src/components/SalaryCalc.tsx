d85ece4d6bdd38d0cada9e7ce269ebb621d3701e
MaheeGamage <mahee.gamage@gmail.com>
2024-02-25 17:54:25 +0530

    Change base template size

--- src/components/SalaryCalc.tsx
+++ src/components/SalaryCalc.tsx
@@ -10,41 +10,39 @@ const SalaryCalc = () => {
 
     return (
         <>
-            <div>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h4" textAlign={"center"}>Salary Calculator</Typography>
-                        <Typography variant="h6" textAlign={"center"} sx={{ mb: 4 }}>(Sri Lanka)</Typography>
-                        <Container sx={{ mb: 4, mt: 4 }}>
-                            <FormControl>
-                                <InputLabel htmlFor="outlined-adornment-amount">Enter your Salary</InputLabel>
-                                <OutlinedInput
-                                    id="outlined-adornment-amount"
-                                    type="number"
-                                    startAdornment={<InputAdornment position="start">Rs</InputAdornment>}
-                                    label="Enter your Salary"
-                                    value={salary}
-                                    onChange={(event) => setSalary(Number(event.target.value))}
-                                />
-                            </FormControl>
-                        </Container>
-                        <Container sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', mb: 2 }}>
-                            <Typography variant="h5">Take Home Salary:</Typography>
-                            {/* <Typography variant="h5">{Math.round(salaryBreakdown.takeHomeSalary).toFixed(2)}</Typography> */}
-                            <Typography variant="h5">{salaryBreakdown.takeHomeSalary.toLocaleString('en')}</Typography>
-                        </Container>
-                        <Container sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                            <Typography variant="h5">Total Deductable</Typography>
-                            <Typography variant="h5">{salaryBreakdown.totalDeduction.toLocaleString('en')}</Typography>
-                            <br />
-                            <Typography variant="h6" fontWeight={'bold'}>Breakdown:</Typography>
-                            <Typography variant="h6">Tax: {salaryBreakdown.tax.toLocaleString('en')}</Typography>
-                            <Typography variant="h6">EPF: {salaryBreakdown.epf.toLocaleString('en')}</Typography>
+            <Card>
+                <CardContent>
+                    <Typography variant="h4" textAlign={"center"}>Salary Calculator</Typography>
+                    <Typography variant="h6" textAlign={"center"} sx={{ mb: 4 }}>(Sri Lanka)</Typography>
+                    <Container sx={{ mb: 4, mt: 4 }}>
+                        <FormControl>
+                            <InputLabel htmlFor="outlined-adornment-amount">Enter your Salary</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                type="number"
+                                startAdornment={<InputAdornment position="start">Rs</InputAdornment>}
+                                label="Enter your Salary"
+                                value={salary}
+                                onChange={(event) => setSalary(Number(event.target.value))}
+                            />
+                        </FormControl>
+                    </Container>
+                    <Container sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', mb: 2 }}>
+                        <Typography variant="h5">Take Home Salary:</Typography>
+                        {/* <Typography variant="h5">{Math.round(salaryBreakdown.takeHomeSalary).toFixed(2)}</Typography> */}
+                        <Typography variant="h5">{salaryBreakdown.takeHomeSalary.toLocaleString('en')}</Typography>
+                    </Container>
+                    <Container sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                        <Typography variant="h5">Total Deductable</Typography>
+                        <Typography variant="h5">{salaryBreakdown.totalDeduction.toLocaleString('en')}</Typography>
+                        <br />
+                        <Typography variant="h6" fontWeight={'bold'}>Breakdown:</Typography>
+                        <Typography variant="h6">Tax: {salaryBreakdown.tax.toLocaleString('en')}</Typography>
+                        <Typography variant="h6">EPF: {salaryBreakdown.epf.toLocaleString('en')}</Typography>
 
-                        </Container>
-                    </CardContent>
-                </Card>
-            </div>
+                    </Container>
+                </CardContent>
+            </Card>
         </>
     )
 }
